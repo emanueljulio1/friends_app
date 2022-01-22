e6fd3582192d3f88936f53a5b7efae3a32aee3d9
Emanuel Melo <emanueljulio1@gmail.com>
2022-01-21 23:02:40 -0300

    Initial commit

---
+++ lib/friends_app/cli/menu_itens.ex
@@ -0,0 +1,16 @@
+defmodule FriendsApp.CLI.MenuItens do
+
+  alias FriendsApp.CLI.Menu
+
+  def all, do: [
+
+    %Menu{label: "Cadastrar um amigo", id: :create},
+    %Menu{label: "Lista os amigos", id: :read},
+    %Menu{label: "Atualiza um amigo", id: :update},
+    %Menu{label: "Exclui um amigo", id: :delete},
+
+
+
+  ]
+
+end
