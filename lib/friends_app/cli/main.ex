6240ed348d8480fb6a6edd3667b5f914a6a79d04
Emanuel Melo <emanueljulio1@gmail.com>
2022-01-22 19:03:51 -0300

    FEAT: Escolhendo um item do menu

--- lib/friends_app/cli/main.ex
+++ lib/friends_app/cli/main.ex
@@ -1,11 +1,13 @@
 defmodule FriendsApp.CLI.Main do
   alias Mix.Shell.IO, as: Shell
+
   def start_app do
     Shell.cmd("clear")
     welcome_message()
     Shell.prompt("Pressione ENTER para continuar")
     starts_menu_choice()
   end
+
   defp welcome_message do
     Shell.info("=========== Friends App ===========")
     Shell.info("Seja bem-vindo à sua agenda pessoal")
@@ -13,8 +15,6 @@ defmodule FriendsApp.CLI.Main do
   end
 
   defp starts_menu_choice do
-    FriendsApp.CLI.MenuChoice.start
-
+    FriendsApp.CLI.MenuChoice.start()
   end
-
 end
